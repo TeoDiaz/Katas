27d14ca25b5a0a48d266bd4adae35be3be76097b
TeoDiaz <tdiazsan@gmail.com>
2019-04-20 03:25:27 +0200

    kata

---
+++ Elixir_7Kyu/Count_the_digit.ex
@@ -0,0 +1,12 @@
+# Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer. Square all numbers k (0 <= k <= n) between 0 and n. Count the numbers of digits d used in the writing of all the k**2. Call nb_dig (or nbDig or ...) the function taking n and d as parameters and returning this count.
+
+defmodule Countdigit do
+
+  def nb_dig(n, d) do
+   list = for n <- 0..n, do: n * n
+   list = for x <- list, do: Integer.digits(x)
+   list = for y <- list, do: Enum.count(y, &(&1 == d))
+   Enum.sum(list)
+  end
+
+end
