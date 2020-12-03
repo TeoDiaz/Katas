7267f1db8b66d4a290b8a05c61912302a229f7e6
Teo Diaz <teo.diaz@cabify.com>
2020-12-03 17:02:03 +0100

    refactor

--- Advent_Of_Code/2020/Day3/Part1.js
+++ Advent_Of_Code/2020/Day3/Part1.js
@@ -17,4 +17,25 @@ function check(input) {
   return trees;
 }
 
+// Refactor
+
+function check(input) {
+  let trees = 0;
+
+  let map = input.split("\n");
+  let pos = 3;
+
+  for (let i = 1; i < map.length; i++) {
+    let last_digit = pos % map[i].length;
+
+    if (map[i][last_digit] == "#") {
+      trees++;
+    }
+
+    pos += 3;
+  }
+
+  return trees;
+}
+
 
