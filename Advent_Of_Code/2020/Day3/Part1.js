ed78d4779c53f0e47b2b5fca34c8c28d10715dc8
Teo Diaz <teo.diaz@cabify.com>
2020-12-03 17:38:39 +0100

    finish part2 Day3

--- Advent_Of_Code/2020/Day3/Part1.js
+++ Advent_Of_Code/2020/Day3/Part1.js
@@ -37,5 +37,3 @@ function check(input) {
 
   return trees;
 }
-
-
---
+++ Advent_Of_Code/2020/Day3/Part2.js
@@ -0,0 +1,28 @@
+function check(input, right, down) {
+  let trees = 0;
+
+  let map = input.split("\n");
+  let pos = right;
+
+  for (let i = down; i < map.length; i += down) {
+    let last_digit = pos % map[i].length;
+
+    if (map[i][last_digit] == "#") {
+      trees++;
+    }
+
+    pos += right;
+  }
+
+  return trees;
+}
+
+function multiply(input) {
+  first = check(input, 1, 1);
+  second = check(input, 3, 1);
+  third = check(input, 5, 1);
+  fourth = check(input, 7, 1);
+  fith = check(input, 1, 2);
+
+  return first * second * third * fourth * fith;
+}
