9b3e4df0427eb4630fa2b005f83339d363e459e3
Teo Diaz <teo.diaz@cabify.com>
2021-12-01 13:45:49 +0100

    add docs

--- Advent_Of_Code/2021/Day1/Part2.js
+++ Advent_Of_Code/2021/Day1/Part2.js
@@ -9,6 +9,12 @@ const lines = fs
 let increase = 0;
 let depth = 3;
 
+/* 
+If numbers are 1 2 3 4 and you compare the first three 1 2 3 and the second three 2 3 4, the 2 3 are irrelevant since they are in both sums. So you only need to compare the first and last number across 1 2 3 4
+
+Source: https://www.reddit.com/r/adventofcode/comments/r66vow/2021_day_1_solutions/hms9f3r/?utm_source=reddit&utm_medium=web2x&context=3
+*/
+
 for (let i = depth; i < lines.length; i++) {
   if (lines[i - depth] < lines[i]) increase++;
 }
