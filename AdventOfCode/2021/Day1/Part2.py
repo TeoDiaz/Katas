9c701063b25a50a9e8b719b16e8fc14ef112f6fd
Teo Diaz <teo.diaz@cabify.com>
2021-12-04 18:28:18 +0100

    add part2 in python

---
+++ AdventOfCode/2021/Day1/Part2.py
@@ -0,0 +1,20 @@
+f = open("day1.txt", "r")
+
+list = f.read()
+list_nums = map(int, list.split("\n"))
+
+
+increase = 0
+past_sum = 0
+next_sum = 0
+
+for indx, val in enumerate(list_nums):
+    if(indx < len(list_nums) - 3):
+        past_sum = list_nums[indx] + list_nums[indx+1] + list_nums[indx + 2]
+        next_sum = list_nums[indx + 1] + \
+            list_nums[indx+2] + list_nums[indx + 3]
+        if(past_sum < next_sum):
+            increase += 1
+
+
+print(increase)
