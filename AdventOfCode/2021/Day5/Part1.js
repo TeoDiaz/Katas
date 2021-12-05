d35594eab18a17d25c7c6fd95051960bd1f6c993
Teo Diaz <teo.diaz@cabify.com>
2021-12-05 13:06:12 +0100

    complete day 5, so proud of this one

---
+++ AdventOfCode/2021/Day5/Part1.js
@@ -0,0 +1,47 @@
+const fs = require("fs");
+
+const lines = fs.readFileSync("day5.txt", { encoding: "utf-8" }).split("\n");
+
+let matrix = new Array(1000).fill(".").map(() => new Array(1000).fill("."));
+
+lines.forEach((z) => {
+  const { groups } =
+    /(?<fromX>\d+),(?<fromY>\d+) -> (?<toX>\d+),(?<toY>\d+)/.exec(z);
+
+  let fromY = parseInt(groups.fromY);
+  let fromX = parseInt(groups.fromX);
+  let toY = parseInt(groups.toY);
+  let toX = parseInt(groups.toX);
+  if (fromX == toX || fromY == toY) {
+    let xPos = [fromX];
+    let yPos = [fromY];
+
+    while (fromX != toX) {
+      fromX < toX ? xPos.push(fromX + 1) : xPos.push(fromX - 1);
+      fromX < toX ? fromX++ : fromX--;
+    }
+
+    while (fromY != toY) {
+      fromY < toY ? yPos.push(fromY + 1) : yPos.push(fromY - 1);
+      fromY < toY ? fromY++ : fromY--;
+    }
+
+    for (let x = 0; x < xPos.length; x++) {
+      for (let y = 0; y < yPos.length; y++) {
+        matrix[yPos[y]][xPos[x]] >= 1
+          ? matrix[yPos[y]][xPos[x]]++
+          : (matrix[yPos[y]][xPos[x]] = 1);
+      }
+    }
+  }
+});
+
+let count = 0;
+
+for (let x = 0; x < matrix.length; x++) {
+  for (let y = 0; y < matrix.length; y++) {
+    matrix[x][y] > 1 ? count++ : count;
+  }
+}
+
+console.log(count);
