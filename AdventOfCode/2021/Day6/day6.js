f998043c206a90d3b8e632fc3e3218e3d74b5ea4
Teo Diaz <teo.diaz@cabify.com>
2021-12-06 10:50:08 +0100

    add day6

---
+++ AdventOfCode/2021/Day6/day6.js
@@ -0,0 +1,35 @@
+// First part I solved it with arrays, but in second part wasn't able because memory, so I'd change the logic
+
+const fs = require("fs");
+
+const initialLanterns = () => {
+  let lanterns = new Array(9).fill(0);
+
+  fs.readFileSync("day6.txt", { encoding: "utf-8" })
+    .split(",")
+    .map((x) => parseInt(x))
+    .forEach((timer) => {
+      lanterns[timer]++;
+    });
+
+  return lanterns;
+};
+
+const produceFish = (lanterns, days) => {
+  let day = 0;
+
+  while (day < days) {
+    let resetIndex = 6;
+    let resetFish = lanterns.shift();
+
+    lanterns[resetIndex] += resetFish;
+    lanterns.push(resetFish);
+
+    day++;
+  }
+
+  return lanterns.reduce((acc, curr) => (acc += curr));
+};
+
+console.log(produceFish(initialLanterns(), 80));
+console.log(produceFish(initialLanterns(), 256));
