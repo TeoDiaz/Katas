581c19b52e885df3d905100dec367d1d0a53a39b
Teo Diaz <teo.diaz@cabify.com>
2021-12-08 17:18:04 +0100

    refactor part 2 and add more comments

--- AdventOfCode/2021/Day8/Part2.js
+++ AdventOfCode/2021/Day8/Part2.js
@@ -20,53 +20,49 @@ let finalCode = [];
 lines.forEach((x) => {
   let mainPattern = x.split(" | ")[0];
   let codePattern = x.split(" | ")[1];
-  // First I set the initialCodes that we are sure this will be 
+
+  // First I set the initialCodes that we are sure there will be, this are 1, 4, 7, 8
   initialCodes(mainPattern);
 
+  // Then I set the number 0, 6 and 9 with length 6
+  setLengthSix(mainPattern);
 
-  // Then I set the number 6, 9 and 0 with length 6
-  mainPattern.split(" ").forEach((code) => {
-    switch (code.length) {
-      case 6:
-        if (!includeChars(code, codes[1])) {
-          codes[6] = code;
-        } else if (includeChars(code, codes[4])) {
-          codes[9] = code;
-        } else {
-          codes[0] = code;
-        }
-
-        break;
-      default:
-        break;
-    }
-  });
-
-  // Then I set the number 3, 5 and 5 with length 5, because they depend on number 6
-  setFive(mainPattern);
+  // Then I set the number 2, 3 and 5 with length 5, because they depend on number 6
+  setLengthFive(mainPattern);
 
   // Once we have all the codes, get the final code
   checkFinalCode(codePattern);
 });
 
-function setFive(mainPattern) {
+function setLengthSix(mainPattern) {
   mainPattern.split(" ").forEach((code) => {
-    switch (code.length) {
-      case 5:
-        if (includeChars(code, codes[1])) {
-          codes[3] = code;
-        } else if (isFive(code, codes[6])) {
-          codes[5] = code;
-        } else {
-          codes[2] = code;
-        }
-        break;
-      default:
-        break;
+    if (code.length == 6) {
+      if (!includeChars(code, codes[1])) {
+        codes[6] = code;
+      } else if (includeChars(code, codes[4])) {
+        codes[9] = code;
+      } else {
+        codes[0] = code;
+      }
     }
   });
 }
 
+function setLengthFive(mainPattern) {
+  mainPattern.split(" ").forEach((code) => {
+    if (code.length == 5) {
+      if (includeChars(code, codes[1])) {
+        codes[3] = code;
+      } else if (isFive(code, codes[6])) {
+        codes[5] = code;
+      } else {
+        codes[2] = code;
+      }
+    }
+  });
+}
+
+// set the codes that have uniq length, 1, 4, 7, 8
 function initialCodes(mainPattern) {
   mainPattern.split(" ").forEach((code) => {
     switch (code.length) {
@@ -88,6 +84,7 @@ function initialCodes(mainPattern) {
   });
 }
 
+// Is 5 if the code have the same length as the number 6 minus 1
 function isFive(code, chars) {
   let count = 0;
 
@@ -100,6 +97,7 @@ function isFive(code, chars) {
   return count == chars.length - 1 ? true : false;
 }
 
+// check if the code includes the chars
 function includeChars(code, chars) {
   let count = 0;
 
@@ -112,6 +110,7 @@ function includeChars(code, chars) {
   return count == chars.length ? true : false;
 }
 
+// check if the code have exactily the same chars, abcd / cdab
 function exactChars(code, chars) {
   let count = 0;
 
@@ -124,21 +123,19 @@ function exactChars(code, chars) {
   return count === code.length && code.length == chars.length ? true : false;
 }
 
+// run the logic for the second part of the input the next to |
 function checkFinalCode(codePattern) {
   let codeNums = [];
-  codePattern.split(" ").forEach((code) => {
-    codeNums.push(checkCode(code));
-  });
-  finalCode.push(codeNums.join(""));
-}
 
-function checkCode(code) {
-  for (const [key, value] of Object.entries(codes)) {
-    if (exactChars(code, value)) {
-      return key;
+  codePattern.split(" ").forEach((code) => {
+    for (const [key, value] of Object.entries(codes)) {
+      if (exactChars(code, value)) {
+        codeNums.push(key);
+      }
     }
-  }
-  return 0;
+  });
+
+  finalCode.push(codeNums.join(""));
 }
 
 console.log(
